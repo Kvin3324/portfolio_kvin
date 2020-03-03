b0fbd587cf8f6c41f3293f21f3885608a553943b
Kévin Joya <kevin.joya@outlook.com>
2020-03-03 18:40:24 +0100

    Created compo Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import HeaderStyled from "./HeaderStyled.style";
+import Languages from "../../utils/languages.json";
+
+function Header(props) {
+  console.log(Languages);
+  const [data, setData] = React.useState({
+    languages: Object.keys(Languages),
+  })
+
+  function changeLanguage(e) {
+    if (props.language === e.target.value) return null;
+    props.callUpdateLanguage(e.target.value);
+  }
+
+  return(
+    <HeaderStyled as="header">
+      <h1>Hello it's me, Kévin</h1>
+        <select onChange={changeLanguage}>
+          {
+            data.languages.map((language, index) => <option key={index}>{language}</option>)
+          }
+        </select>
+      </HeaderStyled>
+  )
+}
+
+export default Header
