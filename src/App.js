0f5f6477bc17160c7aaa470b421b26666604b3b7
Kévin Joya <kevin.joya@outlook.com>
2020-03-03 18:39:56 +0100

    Changed logic and added comp Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
 import Portfolio from './container/Portfolio';
+import Header from './components/Header/Header';
+import Languages from "./utils/languages";
 
 function App() {
+  const [lang, setLang] = React.useState({
+    default: "fr",
+    content: Languages.fr
+  });
+
+  function updateLang(newLanguage) {
+    setLang({
+      default: newLanguage,
+      content: Languages[newLanguage]
+    });
+  }
+
   return (
-    <div className="App">
-      <Portfolio />
-    </div>
-  );
+    <>
+      <Header language={lang.default} callUpdateLanguage={(i) => updateLang(i)} />
+      <Portfolio language={lang.content} />
+    </>
+  )
 }
 
 export default App;
