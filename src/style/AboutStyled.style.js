9067222e3664e607c92b4a6c2710d8afe1c2ed41
Kévin Joya <kevin.joya@outlook.com>
2020-03-04 17:53:06 +0100

    Created compo style for about

---
+++ src/style/AboutStyled.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const AboutStyled = styled.div`
+line-height: 26px;
+font-size: 1.4em;
+  p {
+    font-family: 'Nunito';
+  }
+`
+
+export default AboutStyled
