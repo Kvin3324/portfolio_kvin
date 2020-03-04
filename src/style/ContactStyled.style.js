8da88667cc6df1619af9d729cc69301434ef8dcf
Kévin Joya <kevin.joya@outlook.com>
2020-03-04 17:53:18 +0100

    Created compo contact style

---
+++ src/style/ContactStyled.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const ContactStyled = styled.div`
+margin-top: 40px;
+text-align: left;
+  p {
+    font-family: 'Nunito';
+    font-size: 1.1em;
+  }
+  a {
+    color: rgb(25, 39, 53);
+      &:hover {
+        text-decoration-color: #F0DB4F;
+      }
+  }
+`
+
+export default ContactStyled
