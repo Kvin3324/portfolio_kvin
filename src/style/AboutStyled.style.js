80278855b23fe9c46256860c148aa99b873ba822
Kévin Joya <kevin.joya@outlook.com>
2020-03-05 18:50:32 +0100

    Added responsive

--- src/style/AboutStyled.style.js
+++ src/style/AboutStyled.style.js
@@ -10,6 +10,18 @@ font-size: 1.4em;
   .about--one {
     line-height: 4px;
   }
+
+  @media screen and (min-width: 375px) and (max-width: 812px) {
+    p {
+      line-height: normal!important;
+      font-size: 0.9em!important;
+    }
+  }
+  @media screen and (min-width: 320px) and (max-width: 560px) {
+    p {
+      line-height: normal!important;
+      font-size: 0.7em;
+    }
 `
 
 export default AboutStyled
--- src/style/HeaderStyled.style.js
+++ src/style/HeaderStyled.style.js
@@ -16,6 +16,39 @@ const HeaderStyled = styled(Container)`
     font-family: 'Roboto';
     font-size: 2.5em;
   }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    h1 {
+      font-size: 3em!important;
+    }
+    select {
+      margin-top: 6%!important;
+      margin-left: 20%!important;
+    }
+  }
+
+  @media screen and (min-width: 375px) and (max-width: 812px) {
+    h1 {
+      font-size: 2.1em;
+    }
+    select {
+      width: 10%;
+      margin-left: 1px;
+      margin-top: 0;
+    }
+  }
+
+  @media screen and (min-width: 320px) and (max-width: 560px) {
+    display: inline;
+    h1 {
+      font-size: 1.8em;
+    }
+    select {
+      width: 10%;
+      margin-left: 1px;
+      margin-top: 0;
+    }
+  }
 `
 
 export default HeaderStyled
