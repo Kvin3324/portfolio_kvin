dfa0caef82637e08434691623d5e53af82a9c10a
Kévin Joya <kevin.joya@outlook.com>
2020-11-08 18:01:55 +0100

    Added GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
 import Portfolio from './container/Portfolio';
 import Header from './components/Header/Header';
 import Languages from "./utils/languages";
 
 function App() {
+  const GlobalStyle = createGlobalStyle`
+    body {
+      background-color: #F7F6F7;
+      text-align: center;
+      max-width: 939px;
+      margin: 0 auto;
+      padding: 0 20px;
+    };
+  `;
+
   const [lang, setLang] = React.useState({
     default: "fr",
     content: Languages.fr
@@ -19,6 +29,7 @@ function App() {
 
   return (
     <>
+      <GlobalStyle></GlobalStyle>
       <Header language={lang.default} callUpdateLanguage={(i) => updateLang(i)} />
       <Portfolio language={lang.content} />
     </>
