3d9600e321abae5a66127d92ee0bab09bf27d365
Kévin Joya <kevin.joya@outlook.com>
2020-11-08 18:02:30 +0100

    Added  span with icon & styled file

--- src/container/Portfolio.js
+++ src/container/Portfolio.js
@@ -7,55 +7,87 @@ import Loader from "../components/Loader/Loader";
 
 function Portfolio(props) {
   const [data, setData] = useState({
-    data: []
+    data: [],
   });
 
   useEffect(() => {
     fetch("https://api.github.com/user/repos?sort=pushed&per_page=10", {
       method: "get",
       headers: {
-        "Authorization": `token ${process.env.REACT_APP_APIKEY}`,
+        Authorization: `token ${process.env.REACT_APP_APIKEY}`,
         "User-Agent": "Kvin3324",
-      }
+      },
     })
-      .then(response => response.json())
-      .then(dataParsed => setData({
-        data: dataParsed
-      }))
-      .catch(error => console.log(error))
-  }, [])
+      .then((response) => response.json())
+      .then((dataParsed) =>
+        setData({
+          data: dataParsed,
+        })
+      )
+      .catch((error) => console.log(error));
+  }, []);
 
   if (data.data.length === 0) return <Loader />;
   return (
     <React.Fragment>
       <main>
         <AboutStyled className="about">
-          <p className="about--one">{props.language.about}</p>
+          <div className="about--content">
+            <p className="about--one">
+              {props.language.about}
+              <span>{props.language.iconAbout}</span>
+            </p>
+          </div>
           <p>{props.language.aboutTwo}</p>
         </AboutStyled>
         <ProjectStyled>
           <h4>Repos</h4>
           <ul>
-            {
-              data.data.map((project, index) => {
-                return (
-                  <div key={index}>
-                    <li key={index}>
-                      <a href={project.html_url} target="_blank" rel="noopener noreferrer">{project.name}</a>
-                    </li>
-                    <Chips content={project.language} />
-                  </div>
-                )
-              })
-            }
+            {data.data.map((project, index) => {
+              return (
+                <div key={index}>
+                  <li key={index}>
+                    <a
+                      href={project.html_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {project.name}
+                    </a>
+                  </li>
+                  <Chips content={project.language} />
+                </div>
+              );
+            })}
           </ul>
         </ProjectStyled>
       </main>
       <ContactStyled className="contact">
-        <p>{props.language.contactLinkedin} <a href="https://www.linkedin.com/in/k%C3%A9vin-joya-5b6250133/" target="_blank" rel="noopener noreferrer">Linkedin</a> {props.language.contactGithub} <a href="https://github.com/Kvin3324" target="_blank" rel="noopener noreferrer">Github</a>.<span role="img" aria-label="contact--emoji">📱</span></p>
+        <p>
+          {props.language.contactLinkedin}{" "}
+          <a
+            href="https://www.linkedin.com/in/k%C3%A9vin-joya-5b6250133/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>{" "}
+          {props.language.contactGithub}{" "}
+          <a
+            href="https://github.com/Kvin3324"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          .
+          <span role="img" aria-label="contact--emoji">
+            📱
+          </span>
+        </p>
       </ContactStyled>
     </React.Fragment>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
